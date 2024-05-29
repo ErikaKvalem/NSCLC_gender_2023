5ef16929436e306ec0be14aa35a3530bf048e279
Erika Kvalem <erika.kvalem@student.i-med.ac.at>
2024-05-29 14:03:52 +0200

    venn diagram 2 and 3

--- 02-scripts/40_venn_diagram.R
+++ 02-scripts/40_venn_diagram.R
@@ -1,5 +1,14 @@
 #install.packages("VennDiagram")
 require(data.table)
+# Load library
+library(VennDiagram)
+
+# load ggvenn package 
+library("ggvenn") 
+
+
+# load gplots package 
+library("gplots") 
 
 input_path = "/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/007_re_analysis/tables/deseq2_out/"
 chrom = read_csv("/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/007_re_analysis/tables/input/adata_var_nsclc_chrom.csv")
@@ -16,42 +25,42 @@ tumor_and_normal_deg_merged <- merge(tumor_and_normal_deg, chrom[, c("gene_id", 
 
 # Assuming normal is your dataframe
 normal_genes_X <- normal_deg_merged$gene_id[normal_deg_merged$chromosome_name == "X"]
+normal_genes_X <-na.omit(normal_genes_X)
 normal_genes_Y <- normal_deg_merged$gene_id[normal_deg_merged$chromosome_name == "Y"]
+normal_genes_Y <-na.omit(normal_genes_Y)
 normal_genes_autosomal <- normal_deg_merged$gene_id[!(normal_deg_merged$chromosome_name == "X" | normal_deg_merged$chromosome_name == "Y")]
+normal_genes_autosomal <- na.omit(normal_genes_autosomal)
 
 tumor_genes_X <- tumor_deg_merged$gene_id[tumor_deg_merged$chromosome_name == "X"]
+tumor_genes_X <-na.omit(tumor_genes_X)
 tumor_genes_Y <- tumor_deg_merged$gene_id[tumor_deg_merged$chromosome_name == "Y"]
+tumor_genes_Y <-na.omit(tumor_genes_Y)
 tumor_genes_autosomal <- tumor_deg_merged$gene_id[!(tumor_deg_merged$chromosome_name == "X" | tumor_deg_merged$chromosome_name == "Y")]
+tumor_genes_autosomal <- na.omit(tumor_genes_autosomal)
 
 tumor_and_normal_genes_X <- tumor_and_normal_deg_merged$gene_id[tumor_and_normal_deg_merged$chromosome_name == "X"]
+tumor_and_normal_genes_X <-na.omit(tumor_and_normal_genes_X)
 tumor_and_normal_genes_Y <- tumor_and_normal_deg_merged$gene_id[tumor_and_normal_deg_merged$chromosome_name == "Y"]
+tumor_and_normal_genes_Y <-na.omit(tumor_and_normal_genes_Y)
 tumor_and_normal_genes_autosomal <- tumor_and_normal_deg_merged$gene_id[!(tumor_and_normal_deg_merged$chromosome_name == "X" | tumor_and_normal_deg_merged$chromosome_name == "Y")]
+tumor_and_normal_genes_autosomal <- na.omit(tumor_and_normal_genes_autosomal)
 
 
 
-# Load the required library
-library(VennDiagram)
+########### 2 circles 
+# use list as input 
+X <-list(normal_x = c(normal_genes_X),tumor_x= c(tumor_genes_X)) 
+Y <-list(normal_y = c(normal_genes_Y),tumor_y= c(tumor_genes_Y)) 
+autosomal <- list(normal_autosomal = c(normal_genes_autosomal),tumor_autosomal= c(tumor_genes_autosomal)) 
+# create venn diagram and display all the sets 
+ggvenn(X)
+ggvenn(Y)
+ggvenn(autosomal)
 
-# Define the sets for the Venn diagram
-  normal <- normal_deg_merged$gene_id
-  tumor <- tumor_deg_merged$gene_id
-  tumor_and_normal <- tumor_and_normal_deg_merged$gene_id
+############ 3 circles 
 
-# Create the Venn diagram
-#venn.plot(list(set1 = normal, set2 = tumor, set3 = tumor_and_normal), category.names = c("normal", "tumor", "tumor_and_normal"))
-
-# Create the Venn diagram
-  venn_object <- venn.diagram(
-  x = list(
-    normal = normal,
-    tumor = tumor,
-    tumor_and_normal = tumor_and_normal
-  ),
-  category.names = c("normal", "tumor", "tumor_and_normal"),
-  filename = NULL,  # To prevent saving as a file
-  output = TRUE,    # To output the plot
-  imagetype = "png" # Specify image type if saving as a file
-)
-# Display the plot
-grid.newpage()
-grid.draw(venn_object)
+# creating venn diagram by passing  
+# list as direct parameter 
+venn(list(normal_x=c(normal_genes_X),tumor_x=c(tumor_genes_X),tumor_and_normal_x=c(tumor_and_normal_genes_X)))
+venn(list(normal_y=c(normal_genes_Y),tumor_y=c(tumor_genes_Y),tumor_and_normal_y=c(tumor_and_normal_genes_Y)))
+venn(list(normal_autosomal=c(normal_genes_autosomal),tumor_autosomal=c(tumor_genes_autosomal),tumor_and_normal_autosomal=c(tumor_and_normal_genes_autosomal)))
