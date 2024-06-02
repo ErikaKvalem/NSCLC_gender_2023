df0cc15ce911027cad0bea9ad514c85b3807392e
Erika Kvalem <erika.kvalem@student.i-med.ac.at>
2024-06-02 11:07:26 +0200

    gene expression chrom

--- 02-scripts/50_box_plot_gene_expression_chrom.R
+++ 02-scripts/50_box_plot_gene_expression_chrom.R
@@ -60,188 +60,6 @@ tumor_and_normal_genes_autosomal <- tumor_and_normal_deg_merged$gene_name[!(tumo
 ################################################
 # Convert log1p_norm_counts to dataframe
 df_nc <- as.data.frame(log1p_norm_counts)
-#################################################### NORMAL
-# List of gene names
-index_list <- head(normal_deg$gene_id)
-index_list_name <- head(normal_deg$gene_name)
-
-# Initialize an empty dataframe to store the result
-result <- data.frame()
-# Initialize an empty dataframe to store gene names and p-values
-p_values_df <- data.frame(gene_name = character(), p_val = numeric())
-
-# Iterate over each gene name in index_list
-for (i in seq_along(index_list)) {
-  # Filter dataframe based on gene name
-  df_nc_filtered <- df_nc[index_list[i], ]
-  
-  # Transpose the filtered dataframe
-  df_t <- t(df_nc_filtered)
-  
-  # Convert transposed dataframe to data frame
-  df_t <- as.data.frame(df_t)
-  
-  # Rename the column to 'log1p_norm'
-  colnames(df_t)[colnames(df_t) == index_list[i]] <- 'log1p_norm'
-  
-  # Add 'gene_name' column
-  df_t$gene_id <- as.character(index_list[i])
-  df_t$gene_name <- as.character(index_list_name[i])
-  df_t$sample <- row.names(df_t)
-  df_t <- merge(df_t, samplesheet[, c("sample", "sex", "origin")], by = "sample")
-  # Filter for male
-  male_df <- subset(df_t, sex == "male")
-  
-  # Filter for female
-  female_df <- subset(df_t, sex == "female")
-  
-  # Calculate annotation
-  anno = t.test(male_df$log1p_norm, female_df$log1p_norm)
-  pval = anno$p.value
-  df_t$pval = as.numeric(pval)
-  
-  # Append the result
-  result <- rbind(result, df_t)
-  
-}
-
-######################################################## TUMOR
-# List of gene names
-index_list_tumor <- head(tumor_deg$gene_id)
-index_list_name_tumor <- head(tumor_deg$gene_name)
-
-# Initialize an empty dataframe to store the result
-result2 <- data.frame()
-
-# Iterate over each gene name in index_list
-for (i in seq_along(index_list)) {
-  # Filter dataframe based on gene name
-  df_nc_filtered <- df_nc[index_list_tumor[i], ]
-  
-  # Transpose the filtered dataframe
-  df_t <- t(df_nc_filtered)
-  
-  # Convert transposed dataframe to data frame
-  df_t <- as.data.frame(df_t)
-  
-  # Rename the column to 'log1p_norm'
-  colnames(df_t)[colnames(df_t) == index_list_tumor[i]] <- 'log1p_norm'
-  
-  # Add 'gene_name' column
-  df_t$gene_id <- as.character(index_list_tumor[i])
-  df_t$gene_name <- as.character(index_list_name_tumor[i])
-  df_t$sample <- row.names(df_t)
-  df_t <- merge(df_t, samplesheet[, c("sample", "sex", "origin")], by = "sample")
-  # Filter for male
-  male_df <- subset(df_t, sex == "male")
-  
-  # Filter for female
-  female_df <- subset(df_t, sex == "female")
-  
-  # Calculate annotation
-  anno = t.test(male_df$log1p_norm, female_df$log1p_norm)
-  pval = anno$p.value
-  df_t$pval = as.numeric(pval)
-  
-  # Append the result
-  result2 <- rbind(result2, df_t)
-  
-}
-
-
-####################
-result <- result %>%
-  unite(gn_sex, gene_name, sex, remove=FALSE) 
-
-result2 <- result2 %>%
-  unite(gn_sex, gene_name, sex, remove=FALSE) 
-
-result$condition ="normal_deg"
-result2$condition ="tumor_deg"
-
-results_merged <- rbind(result,result2)
-####################PLOTTING
-
-#NORMAL
-r1 <- result %>%
-  unite(gn_sex, gene_name, sex) %>%
-  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
-  geom_violin() +
-  geom_violin(trim=FALSE)+
-  scale_fill_manual(values=c("red","blue", "red","blue", "red","blue", "red","blue", "red","blue", "red","blue"))+
-  ggsignif::geom_signif(
-    comparisons = list(
-      unique(result$gn_sex)[1:2],
-      unique(result$gn_sex)[3:4],
-      unique(result$gn_sex)[5:6],
-      unique(result$gn_sex)[7:8],
-      unique(result$gn_sex)[9:10],
-      unique(result$gn_sex)[11:12]),y_position = c(11,11,11,11,11,11)
-  )+
-  guides(fill = FALSE)+
-  
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  xlab(NULL)
-
-r1
-
-# Save the plot as an image file (e.g., PNG)
-ggsave(paste0(resDir,"head_normal_deg_violin_plot.jpg"), plot = r1, width = 8, height = 10)  # Adjust width and height as needed
-
-#TUMOR
-r2 <- result2 %>%
-  unite(gn_sex, gene_name, sex) %>%
-  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
-  geom_violin() +
-  geom_violin(trim=FALSE)+
-  scale_fill_manual(values=c("red","blue", "red","blue", "red","blue", "red","blue", "red","blue", "red","blue"))+
-  ggsignif::geom_signif(
-    comparisons = list(
-      unique(result2$gn_sex)[1:2],
-      unique(result2$gn_sex)[3:4],
-      unique(result2$gn_sex)[5:6],
-      unique(result2$gn_sex)[7:8],
-      unique(result2$gn_sex)[9:10],
-      unique(result2$gn_sex)[11:12]),y_position = c(11,11,11,11,11,11)
-  )+
-  guides(fill = FALSE)+
-  
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  xlab(NULL)
-r2
-ggsave(paste0(resDir,"head_tumor_deg_violin_plot.jpg"), plot = r2, width = 8, height = 10)  # Adjust width and height as needed
-
-#NORMAL AND TUMOR 
-p <- results_merged %>%
-  unite(gn_sex, gene_name, sex) %>%
-  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
-  geom_violin() +
-  geom_violin(trim=FALSE)+
-  scale_fill_manual(values=c("red","blue", "red","blue", "red","blue", "red","blue", "red","blue", "red","blue","red","blue", "red","blue", "red","blue", "red","blue", "red","blue"))+
-  ggsignif::geom_signif(
-    comparisons = list(
-      unique(result$gn_sex)[1:2],
-      unique(result$gn_sex)[3:4],
-      unique(result$gn_sex)[5:6],
-      unique(result$gn_sex)[7:8],
-      unique(result$gn_sex)[9:10],
-      unique(result$gn_sex)[11:12],c("AKR1C3_male","AKR1C3_female"),
-      c("KRT19_male","KRT19_female"),
-      c("KRT8_male","KRT8_female"),
-      c("KRT7_male","KRT7_female"),
-      c("KRT17_male","KRT17_female")),
-    y_position = c(11,11,11,11,11,11,11,11,11,11,11)
-  )+
-  guides(fill = FALSE)+
-  
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  xlab(NULL)
-# Use vars() to supply faceting variables:
-p <- p + facet_wrap(vars(condition), scales = "free_y")
-
-
-ggsave(paste0(resDir,"head_tumor_and_normal_deg_violin_plot.jpg"), plot = p, width = 20, height = 10)  # Adjust width and height as needed
-
 
 ###################################################### X CHROM normal
 # List of gene names
@@ -318,7 +136,7 @@ rx <- resultx %>%
 
 rx
 
-ggsave(paste0(resDir,"head_normal_X_deg_violin_plot.jpg"), plot = rx, width = 20, height = 10)  # Adjust width and height as needed
+ggsave(paste0(resDir,"head_normal_X_deg_violin_plot.jpg"), plot = rx, width = 8, height = 10)  # Adjust width and height as needed
 ###################################################### NORMAL Y CHROM
 
 # List of gene names
@@ -380,12 +198,206 @@ ry<- resulty %>%
   scale_fill_manual(values=c("red","blue")) +
   ggsignif::geom_signif(
     comparisons = list(
-      unique(resultx$gn_sex)[1:2],
-      ,y_position = c(11))+
+      unique(resulty$gn_sex)[1:2]
+    ),y_position = c(11)
+  )+
   guides(fill = FALSE)+
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   xlab(NULL)
 
 ry
 
-ggsave(paste0(resDir,"head_normal_Y_deg_violin_plot.jpg"), plot = ry, width = 20, height = 10)  # Adjust width and height as needed
+ggsave(paste0(resDir,"head_normal_Y_deg_violin_plot.jpg"), plot = ry, width = 8, height = 10)  # Adjust width and height as needed
+############################
+############################
+############################
+###################################################### X CHROM tumor
+# List of gene names
+
+tumor_genes_X_id <- tumor_deg_merged$gene_id[tumor_deg_merged$chromosome_name == "X"]
+tumor_genes_X_name <- tumor_deg_merged$gene_name[tumor_deg_merged$chromosome_name == "X"]
+
+index_list <- tumor_genes_X_id[1:6]
+index_list_name <- tumor_genes_X_name[1:6]
+
+# Initialize an empty dataframe to store the result
+resultxt <- data.frame()
+# Initialize an empty dataframe to store gene names and p-values
+#p_values_df <- data.frame(gene_name = character(), p_val = numeric())
+
+# Iterate over each gene name in index_list
+for (i in seq_along(index_list)) {
+  
+  # Filter dataframe based on gene name
+  df_nc_filtered <- df_nc[index_list[i], ]
+  
+  # Transpose the filtered dataframe
+  df_t <- t(df_nc_filtered)
+  
+  # Convert transposed dataframe to data frame
+  df_t <- as.data.frame(df_t)
+  
+  # Rename the column to 'log1p_norm'
+  colnames(df_t)[colnames(df_t) == index_list[i]] <- 'log1p_norm'
+  
+  # Add 'gene_name' column
+  df_t$gene_id <- as.character(index_list[i])
+  df_t$gene_name <- as.character(index_list_name[i])
+  df_t$sample <- row.names(df_t)
+  df_t <- merge(df_t, samplesheet[, c("sample", "sex", "origin")], by = "sample")
+  # Filter for male
+  #male_df <- subset(df_t, sex == "male")
+  
+  # Filter for female
+  #female_df <- subset(df_t, sex == "female")
+  
+  # Calculate annotation
+  #anno = t.test(male_df$log1p_norm, female_df$log1p_norm)
+  #pval = anno$p.value
+  #df_t$pval = as.numeric(pval)
+  
+  # Append the result
+  resultxt <- rbind(resultxt, df_t)
+  
+}
+resultxt <- resultxt %>%
+  unite(gn_sex, gene_name, sex, remove=FALSE) 
+
+resultxt$condition ="tumor_deg_x"
+rxt <- resultxt %>%
+  unite(gn_sex, gene_name, sex) %>%
+  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
+  geom_violin() +
+  geom_violin(trim=FALSE)+
+  scale_fill_manual(values=c("red","blue", "red","blue", "red","blue", "red","blue", "red","blue", "red","blue"))+
+  ggsignif::geom_signif(
+    comparisons = list(
+      unique(resultxt$gn_sex)[1:2],
+      unique(resultxt$gn_sex)[3:4],
+      unique(resultxt$gn_sex)[5:6],
+      unique(resultxt$gn_sex)[7:8],
+      unique(resultxt$gn_sex)[9:10],
+      unique(resultxt$gn_sex)[11:12]),y_position = c(11,11,11,11,11,11)
+  )+
+  guides(fill = FALSE)+
+  
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  xlab(NULL)
+
+rxt
+
+ggsave(paste0(resDir,"head_tumor_X_deg_violin_plot.jpg"), plot = rxt, width = 8, height = 10)  # Adjust width and height as needed
+###################################################### NORMAL Y tumor
+
+# List of gene names
+
+tumor_genes_y_id <- tumor_deg_merged$gene_id[tumor_deg_merged$chromosome_name == "Y"]
+tumor_genes_y_name <- tumor_deg_merged$gene_name[tumor_deg_merged$chromosome_name == "Y"]
+
+index_list <- tumor_genes_y_id
+index_list_name <- tumor_genes_y_name
+
+# Initialize an empty dataframe to store the result
+resultyt <- data.frame()
+# Initialize an empty dataframe to store gene names and p-values
+#p_values_df <- data.frame(gene_name = character(), p_val = numeric())
+
+# Iterate over each gene name in index_list
+for (i in seq_along(index_list)) {
+  # Filter dataframe based on gene name
+  df_nc_filtered <- df_nc[index_list[i], ]
+  
+  # Transpose the filtered dataframe
+  df_t <- t(df_nc_filtered)
+  
+  # Convert transposed dataframe to data frame
+  df_t <- as.data.frame(df_t)
+  
+  # Rename the column to 'log1p_norm'
+  colnames(df_t)[colnames(df_t) == index_list[i]] <- 'log1p_norm'
+  
+  # Add 'gene_name' column
+  df_t$gene_id <- as.character(index_list[i])
+  df_t$gene_name <- as.character(index_list_name[i])
+  df_t$sample <- row.names(df_t)
+  df_t <- merge(df_t, samplesheet[, c("sample", "sex", "origin")], by = "sample")
+  #df_t <- subset(df_t, origin == "tumor_primary")
+
+  # Append the result
+  resultyt<- rbind(resultyt, df_t)
+  
+}
+resultyt <- resultyt %>%
+  unite(gn_sex, gene_name, sex, remove=FALSE) 
+
+resulty$condition ="normal_deg_y"
+ryt<- resultyt %>%
+  unite(gn_sex, gene_name, sex) %>%
+  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
+  geom_violin() +
+  geom_violin(trim=FALSE)+
+  scale_fill_manual(values=c("red","blue")) +
+  ggsignif::geom_signif(
+    comparisons = list(
+      unique(resultyt$gn_sex)[1:2]
+    ),y_position = c(11)
+  )+
+  guides(fill = FALSE)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  xlab(NULL)
+
+ryt
+
+ggsave(paste0(resDir,"head_tumor_Y_deg_violin_plot.jpg"), plot = ryt, width = 8, height = 10)  # Adjust width and height as needed
+
+########################################################################################## facet wrap normal and tumor x 
+
+results_mergedx <- rbind(resultx,resultxt)
+
+#NORMAL AND TUMOR 
+pX <- results_mergedx %>%
+  unite(gn_sex, gene_name, sex) %>%
+  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
+  geom_violin() +
+  geom_violin(trim=FALSE)+
+  scale_fill_manual(values=c("red","blue", "red","blue", "red","blue", "red","blue", "red","blue", "red","blue","red","blue", "red","blue", "red","blue", "red","blue", "red","blue"))+
+  ggsignif::geom_signif(
+    comparisons = list(
+      unique(results_mergedx$gn_sex)[1:2],
+      unique(results_mergedx$gn_sex)[3:4],
+      unique(results_mergedx$gn_sex)[5:6],
+      unique(results_mergedx$gn_sex)[7:8],
+      unique(results_mergedx$gn_sex)[9:10],
+      unique(results_mergedx$gn_sex)[11:12]),
+    y_position = c(11,11,11,11,11,11,11,11,11,11,11)
+  )+
+  guides(fill = FALSE)+
+  
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  xlab(NULL)
+# Use vars() to supply faceting variables:
+pX <- pX + facet_wrap(vars(condition), scales = "free")
+pX
+ggsave(paste0(resDir,"head_normal_and_tumor_x_deg_violin_plot.jpg"), plot = pX, width = 8, height = 10)  # Adjust width and height as needed
+####################################################################################### facet wrap normal and tumor y 
+results_mergedy <- rbind(resulty,resultyt)
+
+pY<- results_mergedy %>%
+  unite(gn_sex, gene_name, sex) %>%
+  ggplot(aes(x = fct_inorder(gn_sex), y = log1p_norm, fill=gn_sex)) +
+  geom_violin() +
+  geom_violin(trim=FALSE)+
+  scale_fill_manual(values=c("red","blue")) +
+  ggsignif::geom_signif(
+    comparisons = list(
+      unique(results_mergedy$gn_sex)[1:2]
+    ),y_position = c(11)
+  )+
+  guides(fill = FALSE)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  xlab(NULL)
+
+# Use vars() to supply faceting variables:
+pY <- pY + facet_wrap(vars(condition), scales = "free")
+pY
+ggsave(paste0(resDir,"head_normal_and_tumor_y_deg_violin_plot.jpg"), plot = pY, width = 8, height = 10)  # Adjust width and height as needed
