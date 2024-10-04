f08dc96bfe56641439f16e8b9af5a959e1df1c00
Erika Kvalem <erika.kvalem@gmail.com>
2024-10-04 14:37:11 +0200

    T reg

--- 02-scripts/011_analysis_paired_remove_xy/20_DESeq2.R
+++ 02-scripts/011_analysis_paired_remove_xy/20_DESeq2.R
@@ -112,8 +112,8 @@ remove_ensg_version = function(x) gsub("\\.[0-9]*$", "", x)
 #
 #
 # DEBUG parameters 
-sampleAnnotationCSV="/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/011_analysis_paired_remove_xy/deseq2_out/pb_cell_type/tumor_vs_normal/macrophage/samplesheet_female_macrophage.csv"
-readCountFile="/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/011_analysis_paired_remove_xy/deseq2_out/pb_cell_type/tumor_vs_normal/macrophage/counts_female_macrophage.csv"
+sampleAnnotationCSV="/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/011_analysis_paired_remove_xy/deseq2_out/pb_cell_type/tumor_vs_normal/regulatory_T_cell/samplesheet_male_regulatory_T_cell.csv"
+readCountFile="/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/011_analysis_paired_remove_xy/deseq2_out/pb_cell_type/tumor_vs_normal/regulatory_T_cell/counts_male_regulatory_T_cell.csv"
 resDir="/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/011_analysis_paired_remove_xy/deseq2_out/out/" 
 resDir_plot="/data/projects/2023/LCBiome/nsclc_gender_atlas_tmp/out/011_analysis_paired_remove_xy/figures/" 
 
@@ -124,7 +124,7 @@ contrast = c(cond_col, c1, c2)
 organism="human"
 n_cpus = 8
 plot_title="DESEQ2"
-prefix = "nsclc_gender_female_macrophage"
+prefix = "nsclc_gender_male_regulatory_T_cell"
 gene_id_type="ENSEMBL"
 sample_col="sample"
 covariate_formula=""
